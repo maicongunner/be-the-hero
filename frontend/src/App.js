831b8dab52bbfa805f717317eecbf8006020893f
maicon.gunner <maikonbdomingues@gmail.com>
2022-03-17 12:46:06 -0300

    feat:test changes the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Routes from './routes';
 import GlobalStyle from './global/style';
 
 import { ToastContainer } from 'react-toastify';
+
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
